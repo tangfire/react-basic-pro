1624849ceb92286ffcd13088bd3de1776f245ec2
tangfire <52628216@qq.com>
2025-11-16 18:31:37 +0800

    Day4 ReactRouter-嵌套路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,25 @@ import Login from '../page/Login';
 
 import {createBrowserRouter} from "react-router-dom";
 import Article from "../page/Article";
+import Layout from "../page/Layout";
+import Board from "../page/Board";
+import About from "../page/About";
 
 const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Layout/>,
+        children: [
+            {
+                path: 'board',
+                element: <Board/>,
+            },
+            {
+                path: 'about',
+                element: <About/>,
+            }
+        ]
+    },
     {
         path: "/login",
         element: <Login />,
