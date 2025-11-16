2f1945f3c2472789209db1c0761f4f003b2de56c
tangfire <52628216@qq.com>
2025-11-16 16:13:59 +0800

    Day4 ReactRouter-路由导航跳转

--- src/page/Login/index.js
+++ src/page/Login/index.js
@@ -1,5 +1,14 @@
+import {Link,useNavigate} from 'react-router-dom';
+
 const Login = () =>{
-    return <div>我是登录页</div>
+    const navigate = useNavigate();
+    return <div>
+        我是登录页
+        {/*声明式写法*/}
+        <Link to="/article">跳转到文章页</Link>
+        {/*命令式写法*/}
+        <button onClick={()=>navigate('/article')}>跳转到文章页</button>
+    </div>
 }
 
 export default Login;
