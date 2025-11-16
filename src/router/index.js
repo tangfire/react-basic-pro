de11c9a6be3fbfd974d20fcadd42f244f3bba8ab
tangfire <52628216@qq.com>
2025-11-16 19:37:25 +0800

    Day4 ReactRouter-两种路由模式

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,13 @@
 import Login from '../page/Login';
 
-import {createBrowserRouter} from "react-router-dom";
+import {createHashRouter} from "react-router-dom";
 import Article from "../page/Article";
 import Layout from "../page/Layout";
 import Board from "../page/Board";
 import About from "../page/About";
 import NotFound from "../page/NotFound";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
     {
         path: '/',
         element: <Layout/>,
